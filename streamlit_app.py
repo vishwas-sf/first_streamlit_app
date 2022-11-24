feace8636be339b298dc0e5a0552fb9c37fed8df
Vishwas Sawant <106583522+vishwas-sf@users.noreply.github.com>
2022-11-24 15:57:28 +0530

    Update streamlit_app.py
    removed line printing json response.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,9 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#streamlit.text(fruityvice_response.json())
 
 # normalize json output to table 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
