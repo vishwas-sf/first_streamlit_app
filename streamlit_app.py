56741eccea35ed74efdce2011fd93e73fe67f32f
Vishwas Sawant <106583522+vishwas-sf@users.noreply.github.com>
2022-11-24 16:04:49 +0530

    Update streamlit_app.py
    added an input text box and assign value of fruit dynamically to get fruit data.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,12 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 # normalize json output to table 
