581f3d5ee336e91b36317a8e946530c897215520
Vishwas Sawant <106583522+vishwas-sf@users.noreply.github.com>
2022-11-25 17:24:37 +0530

    Update streamlit_app.py
    added function for snowflake fetch data and added button.

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,25 +38,26 @@ try:
 except:
   streamlit.error()
     
-#dont run anything past here while we troubleshoot
-streamlit.stop()
-
+#snowflake connections
 import snowflake.connector
-#Snowflake DB Connection
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
-my_cur.execute("SELECT * FROM fruit_load_list")
-#my_data_row = my_cur.fetchone()
-my_data_row = my_cur.fetchall()
-#streamlit.text("The fruit load list contains:")
 streamlit.header("The fruit load list contains:")
-#streamlit.text(my_data_row)
-streamlit.dataframe(my_data_row)
+#snowflake related functions
+def get_fruit_load_list():
+  with my_cnx.cursor as my_cur:
+    my_cur.execute("SELECT * FROM fruit_load_list")
+    return my_cur.fetchall()
+  
+#Add button to load the fruit
+if streamlit.button("Get Fruit Load List"): 
+  #Snowflake DB Connection
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  my_data_row = get_fruit_load_list()
+  streamlit.dataframe(my_data_row)
+  
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
 streamlit.write('Thanks for adding ', add_my_fruit)
+
+#dont run anything past here while we troubleshoot
+streamlit.stop()
 my_cur.execute("insert into pc_rivery_db.public.fruit_load_list values ('from streamlit')")
