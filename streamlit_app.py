b8d0dc8929504a53d00ae734f848f8e7f5628acd
Vishwas Sawant <106583522+vishwas-sf@users.noreply.github.com>
2022-11-25 17:59:52 +0530

    Update streamlit_app.py
    corrected button name for add new fruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ if streamlit.button("Get Fruit Load List"):
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add ?')
 #Add button to add new fruit
-if streamlit.button("Get Fruit Load List"): 
+if streamlit.button("Add a Fruit to the List"): 
   #Snowflake DB Connection
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   back_from_function = insert_row_snowflake(add_my_fruit)
