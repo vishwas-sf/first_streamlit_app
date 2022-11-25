24bfcb82d0350bd4ba8aec74009a0937aa6d550a
Vishwas Sawant <106583522+vishwas-sf@users.noreply.github.com>
2022-11-25 16:12:28 +0530

    Update streamlit_app.py
    changed position of - import reqeust

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,10 @@ streamlit.dataframe(fruits_to_show)
 #New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
+import requests
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
