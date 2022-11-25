494cab180f4b633b4e33943adc011802b074711e
Vishwas Sawant <106583522+vishwas-sf@users.noreply.github.com>
2022-11-25 16:16:26 +0530

    Update streamlit_app.py
    corrected spelling of streamlit at stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,7 +40,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 #dont run anything past here while we troubleshoot
-streadmlit.stop()
+streamlit.stop()
 
 #Snowflake DB Connection
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
